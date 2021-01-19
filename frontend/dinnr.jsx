a4e7692d2e7444ac9177678db4a5cf22e27b59a3
David <davidjhinku@gmail.com>
2021-01-19 13:59:26 -0500

    Root element added

---
+++ frontend/dinnr.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//testing
+//testing
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root')
+
+    //testing
+    //testing
+
+    ReactDOM.render(<h1>React Loaded</h1>, root)
+})
