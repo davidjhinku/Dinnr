e4799add09f9f25790aa22d5c7d9a9da82d555f4
David <davidjhinku@gmail.com>
2021-01-19 20:10:16 -0500

    Created store

--- frontend/dinnr.jsx
+++ frontend/dinnr.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
 
 //testing
 import {signup, signin, logout} from './util/session_api_util'
@@ -7,11 +8,13 @@ import {signup, signin, logout} from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
+    const store = configureStore()
 
     //testing
     window.signup = signup
     window.signin = signin
     window.logout = logout
+    window.getState = store.getState
     //testing
 
     ReactDOM.render(<h1>React Loaded</h1>, root)
