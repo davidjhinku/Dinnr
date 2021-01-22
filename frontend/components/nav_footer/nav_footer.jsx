990da595ffb20ea79ea7c9185c848f49cabef912
David <davidjhinku@gmail.com>
2021-01-21 20:57:03 -0500

    Completed footer

--- frontend/components/nav_footer/nav_footer.jsx
+++ frontend/components/nav_footer/nav_footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NavFooter = () => {
     return(
-        <div>
+        <div className='footer-connect'>
             <h3>David Jhinku - Connect</h3>
-            <hr/>
-            <ul>
+            <hr className="footer-hr"/>
+            <ul className="social-links">
                 <li>
                     <a href="https://github.com/davidjhinku"><img src={window.githubUrl} className='footer-icon' /></a>
                 </li>
