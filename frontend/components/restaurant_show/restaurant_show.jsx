d367c464ed059d18af403bb20041b3aaead55d2f
David <davidjhinku@gmail.com>
2021-01-24 16:50:14 -0500

    Fixed restaurant show page refresh error

--- frontend/components/restaurant_show/restaurant_show.jsx
+++ frontend/components/restaurant_show/restaurant_show.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 
 class RestaurantShow extends React.Component {
-
+    constructor(props){
+        super(props);
+    }
     componentDidMount(){
         this.props.fetchRestaurant(this.props.restId)
     }
 
+    componentWillUnmount() {
+        console.log('it unmounted')
+    }
+
     render(){
         const restaurant = this.props.restaurant
-        return(
-            <div>
-                This is the {restaurant.name} show page
-            </div>
-        )
+
+        if (!restaurant) {
+            return (
+                <div>Loading Restaurant...</div>
+            )
+        } else {
+            return (
+                <div>
+                    This is the {restaurant.name} show page
+                </div>
+            )
+        }
     }
 }
 
