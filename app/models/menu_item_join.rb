4bfeea0e743547dfcc81d8844b9b221aec3a5908
David <davidjhinku@gmail.com>
2021-01-25 10:09:05 -0500

    Fixed second model syntax error

--- app/models/menu_item_join.rb
+++ app/models/menu_item_join.rb
@@ -1,12 +1,12 @@
 class MenuItemJoin < ApplicationRecord
     validates :menu_id, :item_id, presence: true
 
-    belongs_to :menu
+    belongs_to :menu,
         class_name: :Menu,
         foreign_key: :menu_id,
         primary_key: :id
     
-    belongs_to :item
+    belongs_to :item,
         class_name: :Item,
         foreign_key: :item_id,
         primary_key: :id
