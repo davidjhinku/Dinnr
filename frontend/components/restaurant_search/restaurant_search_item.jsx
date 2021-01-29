a0cd1710f89db38be5bfbcda8178750ecd5c676e
David <davidjhinku@gmail.com>
2021-01-28 21:56:34 -0500

    Switched to production photo code

--- frontend/components/restaurant_search/restaurant_search_item.jsx
+++ frontend/components/restaurant_search/restaurant_search_item.jsx
@@ -18,8 +18,8 @@ class RestaurantSearchItem extends React.Component {
         return(
             <li>
                 <div className='search-restaurant-item'>
-                    <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="test image" onClick={this.handleClick}/>
-                    {/* <img src={rest.photoUrl} onClick={this.handleClick}/> */}
+                    {/* <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="test image" onClick={this.handleClick}/> */}
+                    <img src={rest.photoUrl} onClick={this.handleClick}/>
 
                     <div className='rest-information'>
                         <p onClick={this.handleClick}>{rest.name}</p>
