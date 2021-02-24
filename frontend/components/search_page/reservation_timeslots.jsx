4ad9010d05553549aef2c827e52dd65e8db363cb
David <davidjhinku@gmail.com>
2021-02-23 20:52:54 -0500

    Setup skeleton for reservation page

--- frontend/components/search_page/reservation_timeslots.jsx
+++ frontend/components/search_page/reservation_timeslots.jsx
@@ -4,7 +4,7 @@ import { numberToTime, timeSlotLi } from '../../util/util_functions'
 class ReservationTimeslots extends React.Component {
 
     componentDidMount() {
-        //function to fetch the reservation slots
+        this.props.checkTimeslots(this.props.searchParams)
     }
     
     componentDidUnmount(){
--- frontend/components/search_page/reservation_timeslots_container.js
+++ frontend/components/search_page/reservation_timeslots_container.js
@@ -1,11 +1,12 @@
 import ReservationTimeslots from './reservation_timeslots'
 import { connect } from 'react-redux'
-import {  } from '../../actions/reservation_actions'
+import { checkTimeslots } from '../../actions/reservation_actions'
 
 const mSTP = (state, ownProps) => {
     debugger
     return {
         searchParams: state.entities.search_params,
+        timeslots: state.entities.timeslots
         //potentially some default state for timeslots from reducers
         // restaurants: state.entities.restaurants
     }
@@ -13,6 +14,7 @@ const mSTP = (state, ownProps) => {
 
 const mDTP = dispatch => {
     return {
+        checkTimeslots: search => dispatch(checkTimeslots(search))
         //something making a b/e request for reservations
         // fetchRestaurants: search => dispatch(fetchRestaurants(search))
     }
