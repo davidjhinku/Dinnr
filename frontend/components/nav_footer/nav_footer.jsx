9796eea2ac570cd68ce3f6a3aee6214a6b433ec6
David <davidjhinku@gmail.com>
2021-03-01 18:49:16 -0500

    Working on new footer styling

--- frontend/components/nav_footer/nav_footer.jsx
+++ frontend/components/nav_footer/nav_footer.jsx
@@ -3,16 +3,33 @@ import React from 'react'
 const NavFooter = () => {
     return(
         <div className='footer-connect'>
-            <h3>David Jhinku - Connect</h3>
+            <div className='top'>
+                <h3 className='my-name'>David Jhinku</h3>
+                <div className="social-links">
+                    <h4>Join me on</h4>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/davidjhinku" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+                            {/* <a href="https://github.com/davidjhinku"><img src={window.githubUrl} className='footer-icon' /></a> */}
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/davidjhinku/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+                            {/* <a href="https://www.linkedin.com/in/davidjhinku/"><img src={window.linkedinUrl} className='footer-icon' /></a> */}
+                        </li>
+                        <li>
+                            <a href="https://angel.co/u/david-jhinku" target="_blank"><i class="fab fa-angellist fa-2x"></i></a>
+                        </li>
+                        <li>
+                            <a href="mailto: davidjhinku@gmail.com" target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
             <hr className="footer-hr"/>
-            <ul className="social-links">
-                <li>
-                    <a href="https://github.com/davidjhinku"><img src={window.githubUrl} className='footer-icon' /></a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/davidjhinku/"><img src={window.linkedinUrl} className='footer-icon' /></a>
-                </li>
-            </ul>
+            <p id="footer-copyright">
+                Probably not Copyright &copy; 2021 Dinnr
+            </p>
         </div>
     )
 }
