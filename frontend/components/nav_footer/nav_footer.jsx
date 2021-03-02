cdf456fbd3a11640f3f7291604a6d135bcfe3b65
David <davidjhinku@gmail.com>
2021-03-01 19:36:41 -0500

    Added new footer styling

--- frontend/components/nav_footer/nav_footer.jsx
+++ frontend/components/nav_footer/nav_footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavFooter = () => {
     return(
-        <div className='footer-connect'>
+        <div className='footer'>
             <div className='top'>
                 <h3 className='my-name'>David Jhinku</h3>
                 <div className="social-links">
@@ -10,11 +10,9 @@ const NavFooter = () => {
                     <ul>
                         <li>
                             <a href="https://github.com/davidjhinku" target="_blank"><i class="fab fa-github fa-2x"></i></a>
-                            {/* <a href="https://github.com/davidjhinku"><img src={window.githubUrl} className='footer-icon' /></a> */}
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/davidjhinku/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
-                            {/* <a href="https://www.linkedin.com/in/davidjhinku/"><img src={window.linkedinUrl} className='footer-icon' /></a> */}
                         </li>
                         <li>
                             <a href="https://angel.co/u/david-jhinku" target="_blank"><i class="fab fa-angellist fa-2x"></i></a>
