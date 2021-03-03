def9358213531f928951dab3c6efe2e838b443f7
David <davidjhinku@gmail.com>
2021-03-03 16:54:00 -0500

    Setup page one of review form

--- frontend/components/review_form/review_form.jsx
+++ frontend/components/review_form/review_form.jsx
@@ -26,6 +26,39 @@ class ReviewForm extends React.Component {
         }
     }
 
+    radioStars(type) {
+        return(
+            <div className='row'>
+                <div className='label'>
+                    <label htmlFor={type}>{type}</label>
+                </div>
+                <div>
+                    <input type="radio" name={type} id="stars5" value="5" />
+                    <input type="radio" name={type} id="stars4" value="4" />
+                    <input type="radio" name={type} id="stars3" value="3" />
+                    <input type="radio" name={type} id="stars2" value="2" />
+                    <input type="radio" name={type} id="stars1" value="1" />
+                </div>
+            </div>
+        )
+    }
+
+    radioBars() {
+        return (
+            <div className='row'>
+                <div className='label'>
+                    <label htmlFor='noise'>Noise level</label>
+                </div>
+                <div>
+                    <input type="radio" name='noise' id="bars4" value="4" />
+                    <input type="radio" name='noise' id="bars3" value="3" />
+                    <input type="radio" name='noise' id="bars2" value="2" />
+                    <input type="radio" name='noise' id="bars1" value="1" />
+                </div>
+            </div>
+        )
+    }
+
     render() {
         debugger
         let currentUser = this.props.currentUser
@@ -34,15 +67,37 @@ class ReviewForm extends React.Component {
 
         debugger
 
-        // if (this.props.resId !== restaurant.id || this.props.restId !== reservation.it) {
-        if (reservation["default"] || this.props.resId !== restaurant.id || this.props.restId !== reservation.it) {
+        // if (this.props.resId !== restaurant.id || this.props.restId !== reservation.id) {
+        if (reservation["default"] || this.props.resId != reservation.id || this.props.restId != restaurant.id) {
             return(
                 <div>Loading...</div>
             )
         } else {
             return(
-                <div>
-                    Review Form
+                <div className='review-page'>
+                    <div className='form-container'>
+                        <form action="">
+                            <div className='page'>
+                                <div className='header'>
+                                    <h1><span className='capitalize'>{currentUser.first_name}</span>, how was your experience at {restaurant.name}</h1>
+                                    <p>Rate your dining experience (required)</p>
+                                    <p>Reservation made on {reservation.date}</p>
+                                </div>
+
+                                <div className='radio-ratings'>
+                                    {this.radioStars('overall')}
+                                    {this.radioStars('food')}
+                                    {this.radioStars('service')}
+                                    {this.radioStars('ambiance')}
+                                    {this.radioStars('value')}
+                                    {this.radioBars()}
+
+                                </div>
+                                <button>Next</button>
+                            </div>
+                        </form>
+
+                    </div>
                 </div>
             )
         }
